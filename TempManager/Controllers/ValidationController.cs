917abece3d822bb293034d8cc417ba3b4ded13b2
ishani216 <31428274+ishani216@users.noreply.github.com>
2021-04-04 22:46:41 -0400

    Add project files.

---
+++ TempManager/Controllers/ValidationController.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TempManager.Models;
+
+namespace TempManager.Controllers
+{
+    public class ValidationController : Controller
+    {
+        private TempManagerContext context;
+        public ValidationController(TempManagerContext ctx) => context = ctx;
+
+        public JsonResult CheckDate(string date)
+        {
+            DateTime dateTime = Convert.ToDateTime(date);
+            var check = context.Temps.FirstOrDefault(
+                t => t.Date == dateTime);
+            if (check == null)
+            {
+                return Json(true);
+            }
+            else
+            {
+                return Json("Date already entered.");
+            }
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
